782bd0113675fe6bc733ba2e81462a953b14e80c
znamteam-max <znamteam@gmail.com>
2026-09-03 15:54:01 +0300

    Remove Content Core attribution push from reporting bot

--- api/cron/process-jobs.py
+++ api/cron/process-jobs.py
@@ -6,7 +6,6 @@ from http.server import BaseHTTPRequestHandler
 from typing import Any
 
 from bot import (
-    content_core_attribution,
     content_core_integration,
     content_core_multicore,
     project_workflow_patch,
@@ -18,7 +17,6 @@ from bot.job_worker import process_jobs
 
 project_workflow_patch.install()
 content_core_multicore.install()
-content_core_attribution.install()
 content_core_integration.install_worker()
 
 
